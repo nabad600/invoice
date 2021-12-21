b28eb8978802b91bc93d75660d764ecfc5b0854e
Naba Das <nabad600@gmail.com>
2021-12-21 14:54:52 +0530

    CMD add

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -3,9 +3,9 @@
 
 if [ ! -d /var/www/app ] ; then
   echo "Trying to copy...."
-  
   cp -av /tmp/ninja-src/* /var/www/
   rm -R /tmp/ninja*
+  echo "Configurtion add"
   cp /app/default.conf /etc/nginx/conf.d/default.conf
   nginx -s reload
 else
